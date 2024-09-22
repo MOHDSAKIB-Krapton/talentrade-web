4a5bb0f62a22cf8963bc447f28edc2ee26631084
Mohd Sakib <mohdsakib@krapton.com>
2024-09-23 00:17:37 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { Navbar, HeroSection, Footer, Features, Contact } from './components';
-import { Skill } from './assets';
+import { Navbar, HeroSection, Footer, Features, Contact } from "./components";
+import { Skill } from "./assets";
 
 function App() {
-
   return (
     <>
-    <Navbar />
-      <main className='max-w-[1920px] min-h-screen mx-auto'>
+      <Navbar />
+      <main className="max-w-[1920px] min-h-screen mx-auto">
         <HeroSection />
         <Features />
         <Features />
@@ -16,7 +15,7 @@ function App() {
         <Footer />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
