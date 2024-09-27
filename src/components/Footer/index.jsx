37d03816ad009c018e45493c3400ea3961c67e12
Mohd Sakib <mohdsakib@krapton.com>
2024-09-27 14:06:19 +0530

    Footer responsiveness

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -35,10 +35,12 @@ function Footer() {
   };
 
   return (
-    <footer className=" w-full relative">
-      <div className="max-w-7xl mx-auto px-4 pb-4 flex flex-col xl:flex-row">
-        <div className="flex flex-col justify-center items-start">
-          <Name width={230} height={100} />
+    <footer className=" w-full relative border-t border-gray-200 bg-gray-100">
+      <div className="max-w-7xl mx-auto px-4 py-10 flex flex-col xl:flex-row xl:justify-between">
+        <div className="flex flex-col justify-start items-start xl:max-w-96">
+          <div className="">
+            <Name width={230} height={100} />
+          </div>
           <p className="opacity-80 font-semibold text-sm text-start sm:text-start">
             TalentTrade is a global platform where individuals come together to
             learn, teach, and share new skills. Our mission is to empower
@@ -47,50 +49,55 @@ function Footer() {
           </p>
           <div className="space-x-4 my-6 ">
             <InstagramIcon
-              className="hover:text-[#C13584]"
+              className="hover:text-[#C13584] cursor-pointer"
               style={{ fontSize: "1.5rem" }}
             />
             <GitHubIcon
-              className="hover:text-[#833AB4]"
+              className="hover:text-[#833AB4] cursor-pointer"
               style={{ fontSize: "1.5rem" }}
             />
             <LinkedInIcon
-              className="hover:text-[#0a66c2]"
+              className="hover:text-[#0a66c2] cursor-pointer"
               style={{ fontSize: "1.5rem" }}
             />
-            <XIcon style={{ fontSize: "1.3rem" }} />
+            <XIcon
+              className="hover:text-black cursor-pointer"
+              style={{ fontSize: "1.3rem" }}
+            />
           </div>
         </div>
 
-        <div className="grid grid-cols-2 md:grid-cols-4 opacity-80 font-normal space-y-4 p-1 mb-20">
-          {Object.keys(footerLinks).map((category, categoryIndex) => (
-            <div key={categoryIndex} className="mb-4">
-              <h4 className="text-lg font-semibold text-gray-700 mb-2">
-                {category}
-              </h4>
-              <div className="flex flex-col gap-4">
-                {footerLinks[category].map((link, linkIndex) => (
-                  <a
-                    key={linkIndex}
-                    href={link.href}
-                    className="hover:text-purple-700 text-xs 2xl:text-sm text-gray-600 font-medium hover:underline cursor-pointer"
-                  >
-                    {link.name}
-                  </a>
-                ))}
+        <div className=" opacity-80 xl:flex xl:items-center xl:px-4 ">
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-20 xl:mb-0">
+            {Object.keys(footerLinks).map((category, categoryIndex) => (
+              <div key={categoryIndex} className="mb-4">
+                <h4 className="text-lg font-semibold text-gray-700 mb-2">
+                  {category}
+                </h4>
+                <div className="flex flex-col gap-4">
+                  {footerLinks[category].map((link, linkIndex) => (
+                    <a
+                      key={linkIndex}
+                      href={link.href}
+                      className="hover:text-purple-700 text-xs text-gray-600 font-medium hover:underline cursor-pointer"
+                    >
+                      {link.name}
+                    </a>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
 
       <img
         src={flower}
-        className="bottom-[39px] right-12 absolute -z-10"
+        className="bottom-10 right-12 absolute z-10"
         alt="flower image"
       />
 
-      <div className="w-full bg-purple-500 h-12 -z-20 flex items-center justify-start">
+      <div className="w-full bg-purple-500 h-12 z-20 flex items-center justify-start">
         <div className="max-w-7xl px-4 mx-auto">
           <p className="text-white text-xs lg:text-sm font-normal">
             © 2024 TalentTrade. All rights reserved.{" "}
